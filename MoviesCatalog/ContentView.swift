fc04618f6b93c0a832abc0566f6b4b72e3f593a0
Arsentiy <82136438+TarkWight@users.noreply.github.com>
2023-12-24 15:54:50 +0600

    [Develop][Init] Initialized a new SwiftUI project and configured .gitignore
    Created a new empty project using SwiftUI on the main branch.
    Set up the initial project files and directories for SwiftUI development.
    Configured the .gitignore file to exclude unnecessary and temporary files.

---
+++ MoviesCatalog/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MoviesCatalog
+//
+//  Created by Arsentiy on 15.12.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
