70bc976ec33c4350d566e59214a9f7151edfa812
Arsentiy <82136438+TarkWight@users.noreply.github.com>
2023-12-28 18:15:15 +0600

    [UI]Complete LoginScreenView
    -- Added color for IOS-status-bar and HomeIndicator
    -- Deleted TextField and SecureField paddings

--- MoviesCatalog/Views/LoginScreenView.swift
+++ MoviesCatalog/Views/LoginScreenView.swift
@@ -10,76 +10,105 @@ import SwiftUI
 struct LoginScreenView: View {
     @ObservedObject var viewModel: LoginViewModel
     @ObservedObject var coordinator: LoginCoordinator
-
+    
+    @State private var isPasswordVisible = false
+    
     var body: some View {
-        VStack {
-            // MARK: First Frame
-            HStack {
-                backButton
-                    .padding(.leading, 15)
-                
-                Spacer()
-                
-                Text("FИЛЬМУС")
-                    .font(.title2)
-                    .bold()
-                    .padding(.trailing, 15)
-                    .foregroundColor(EnumColors.accent)
-                
-                Spacer()
-            }
-            .padding(.top, 15)
-            
-          // MARK: Second Frame (Label + TextField + Button)
+        ZStack {
+            EnumColors.gray
+                .ignoresSafeArea()
             VStack {
-                Text("ВХОД")
-                    .font(.title)
-                    .padding()
-
-                TextField("Логин", text: $viewModel.username)
-                    .textFieldStyle(RoundedBorderTextFieldStyle())
-                    .padding()
-
-                SecureField("Пароль", text: $viewModel.password)
-                    .textFieldStyle(RoundedBorderTextFieldStyle())
-                    .padding()
-
-                Button("Войти") {
-                    viewModel.login()
+                // MARK: First Frame
+                HStack {
+                    backButton
+                        .padding(.leading, 15)
+                    
+                    Spacer()
+                    
+                    Text("FИЛЬМУС")
+                        .font(.title2)
+                        .bold()
+                        .padding(.trailing, 15)
+                        .foregroundColor(EnumColors.accent)
+                    
+                    Spacer()
                 }
-                .padding()
-            }
-            .padding()
-
-            // MARK: Fifth Frame (Combined Text and Button)
-            HStack {
-                Text("Еще нет аккаунта?")
+                .padding(.leading, 15)
                 
-                Button("Зарегистрируйтесь") {
-                    // Используйте метод goToRegistration для перехода на экран регистрации
-                    coordinator.goToRegistration()
+                // MARK: Second Frame
+                VStack {
+                    Text("ВХОД")
+                        .font(.title)
+                        .bold()
+                        .padding()
+                        .foregroundColor(.white)
+                    
+                    Text("Логин")
+                        .foregroundColor(.white)
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                    TextField("Введите логин", text: $viewModel.username)
+                        .textFieldStyle(RoundedBorderTextFieldStyle())
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                    
+                    Text("Пароль")
+                        .foregroundColor(.white)
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                    
+                    ZStack(alignment: .trailing) {
+                        if isPasswordVisible {
+                            TextField("Введите пароль", text: $viewModel.password)
+                                .textFieldStyle(RoundedBorderTextFieldStyle())
+                        } else {
+                            SecureField("Введите пароль", text: $viewModel.password)
+                                .textFieldStyle(RoundedBorderTextFieldStyle())
+                        }
+                        
+                        Button(action: {
+                            isPasswordVisible.toggle()
+                        }) {
+                            Image(systemName: isPasswordVisible ? "eye.fill" : "eye.slash.fill")
+                                .foregroundColor(EnumColors.getColor(of: EnumColors.gray, by: 0.45))
+                        }
+                        .padding(.trailing, 8)
+                    }
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    
+                    Button("Войти") {
+                        viewModel.login()
+                    }
+                    .buttonStyle(CustomButtonStyle(color: EnumColors.accent, textColor: .white))
+                    .padding(.vertical, 20)
                 }
                 .padding()
+                Spacer()
+                
+                // MARK: Fifth Frame
+                HStack {
+                    Text("Еще нет аккаунта?")
+                    
+                    Button("Зарегистрируйтесь") {
+                        coordinator.goToRegistration()
+                    }
+                    .padding()
+                }
+                .padding()
+                
             }
-            .padding()
-
-            Spacer()
         }
-        .padding()
-        .navigationTitle("Login")
-        .navigationBarBackButtonHidden(true)
     }
-
+    
     private var backButton: some View {
         Button(action: {
             coordinator.goBackToAuthenticationChoice()
         }) {
             Image(systemName: "chevron.left")
                 .font(.title2)
-                .foregroundColor(.blue)
+                .foregroundColor(.white)
         }
     }
 }
+
+
 
 struct LoginScreenView_Previews: PreviewProvider {
     static var previews: some View {
