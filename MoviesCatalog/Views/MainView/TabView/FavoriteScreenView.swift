85e24295e87f553745fc6644c5ff79db679520f0
Arsentiy <82136438+TarkWight@users.noreply.github.com>
2023-12-29 23:56:43 +0600

    [UI][TabView] Create FavoriteScreenView
    -- Create FavoriteScreenView for MainScreen (Third View)

---
+++ MoviesCatalog/Views/MainView/TabView/FavoriteScreenView.swift
@@ -0,0 +1,18 @@
+//
+//  FavoriteScreenView
+//  MoviesCatalog
+//
+//  Created by: Arsentiy
+//
+
+import SwiftUI
+
+struct FavoriteScreenView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FavoriteScreenView()
+}
